696f9d55e04c8ca491c0e836e50873132cbe2f03
Paulo Silva <pauloecomercial367@gmail.com>
2025-03-02 11:17:34 -0300

    :sparkles: modelos do projeto

--- blogsystem/blog/models.py
+++ blogsystem/blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Base(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    class Meta:
+        abstract = True
+
+class Category(Base):
+    name = models.CharField(max_length=255)
+    
+class Post(Base):
+    title = models.CharField(max_length=255)
+    post_text = models.TextField()
+    main_photo = models.ImageField(upload_to='main-photo/')
+    category = models.ManyToManyField(Category, related_name='posts')
+    
+class PostPhoto(Base):
+    path = models.ImageField()
